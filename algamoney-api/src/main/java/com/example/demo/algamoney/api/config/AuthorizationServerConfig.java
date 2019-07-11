642a8c0414d78618481500433e03e918cc65e89f
mgs17 <mgs17@DESKTOP-JN84MM9>
2019-07-11 12:52:35 -0300

    Adicionando o CORS e incluindo o Usuario ao Banco

--- algamoney-api/src/main/java/com/example/demo/algamoney/api/config/AuthorizationServerConfig.java
+++ algamoney-api/src/main/java/com/example/demo/algamoney/api/config/AuthorizationServerConfig.java
@@ -27,7 +27,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.secret("@ngul@r0")
 			.scopes("read","write")
 			.authorizedGrantTypes("password", "refresh_token")
-			.accessTokenValiditySeconds(20)
+			.accessTokenValiditySeconds(2000)
 			.refreshTokenValiditySeconds(3600 * 24); 
 	}
 	
