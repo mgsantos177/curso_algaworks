87fd40cb3d5c9897f488948603ced071a1e2dd29
Matheus Gonçalves <mgsantos177@gmail.com>
2019-07-18 08:40:21 -0300

    removendo arquivos

--- src/main/java/com/example/demo/algamoney/api/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.algamoney.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.algamoney.api.model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
-
-}
